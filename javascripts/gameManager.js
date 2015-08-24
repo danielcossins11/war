dd04d80d13d15090449bd9b40a70d5f4153c0a08
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-24 18:10:14 -0500

    split arrays and have game logic but its broken

--- javascripts/gameManager.js
+++ javascripts/gameManager.js
@@ -3,103 +3,71 @@ define(function(require){
   var Q = require("q");
   var promise = require("getAPI");
 
-  var count = 0;
-  var count1 = 0;
-  var count2 = 0;
-  var deck1_id, deck2_id, deck1_remaining, deck2_remaining;
-  var card1 = {};
-  var card2 = {};
+  var cardArray = [];
+  var p1Array = [];
+  var p2Array = [];
+  var randNum;
 
   var deck1 = promise("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
   deck1.then(function(data){
-    console.log(data);
-    deck1_id = data.deck_id;
-    deck1_remaining = data.remaining;
-    console.log("ID1",deck1_id);
+    var draw = promise("http://deckofcardsapi.com/api/deck/"+data.deck_id+"/draw/?count=52");
+    draw.then(function(data){
+      cardArray = data.cards;
+    });
   });
 
-  var deck2 = promise("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-  deck2.then(function(data2){
-    console.log(data2);
-    deck2_id = data2.deck_id;
-    deck2_remaining = data2.remaining;
-    console.log("ID2", deck2_id);
-  });
+  for(var i=0; i<cardArray.length; i++){
+    if(cardArray.value==="JACK"){
+      cardArray.value = "11";
+    } else if(cardArray.value==="QUEEN"){
+      cardArray.value = "12";
+    } else if(cardArray.value==="KING"){
+      cardArray.value = "13";
+    } else if(cardArray.value==="ACE"){
+      cardArray.value = "14";
+    }  
+  }
+  
 
   $("#draw").on("click", function(){
-    if(count<52){
-      var draw1 = promise("http://deckofcardsapi.com/api/deck/"+deck1_id+"/draw/?count=1");
-      draw1.then(function(data){
-        $("#player1").html("<img src='"+data.cards[0].image+"'><br><h3>P1 count: "+count1+"</h3>");
-
-
-
-
-
-
-        console.log(data);
-        card1 = {
-          value: data.cards[0].value,
-          suit: data.cards[0].suit
-        };
-        console.log("card2", card1);
-
-        if(card1.value==="JACK"){
-          card1.value = "11";
-        } else if(card1.value==="QUEEN"){
-          card1.value = "12";
-        } else if(card1.value==="KING"){
-          card1.value = "13";
-        } else if(card1.value==="ACE"){
-          card1.value = "14";
-        }
-
-
-        var draw2 = promise("http://deckofcardsapi.com/api/deck/"+deck2_id+"/draw/?count=1");
-          draw2.then(function(data){
-          $("#player2").html("<img src='"+data.cards[0].image+"'><br><h3>P2 count: "+count2+"</h3>");
-          console.log(data);
-          card2 = {
-            value: data.cards[0].value,
-            suit: data.cards[0].suit
-          };
-          console.log("card2", card2);
-
-          if(card2.value==="JACK"){
-            card2.value = "11";
-          } else if(card2.value==="QUEEN"){
-            card2.value = "12";
-          } else if(card2.value==="KING"){
-            card2.value = "13";
-          } else if(card2.value==="ACE"){
-            card2.value = "14";
-          }
-
-          ////////CHECK WHICH CARD IS HIGHER
-
-          if(card1.value>card2.value){
-            count1++;
-            if(count>=51){
-              if(count1>count2){
-                console.log("player 1 wins");
-              }
-            }
-          } else if(card2.value>card1.value){
-            count2++;
-            if(count>=51){
-              if(count2>count1){
-                console.log("player 2 wins");
-              }
-            }
-          } else{
-            console.log("It's a draw");
-          }
-          count++;
-          console.log("turn count", count);
-          console.log("count1", count1);
-          console.log("count2", count2);
-        });
-      });
+    console.log(cardArray);
+    for(var i=0; i<52; i++){
+      var num;
+      if(i%2===0){
+      num = Math.floor(Math.random()*(cardArray.length));
+      p1Array[i] = cardArray[num];
+      cardArray.splice(num, 1);
+      } else{
+        num = Math.floor(Math.random()*(cardArray.length));
+        p2Array[i] = cardArray[num];
+        cardArray.splice(num, 1);
+      }
     }
+    console.log(cardArray, p1Array, p2Array);
+
+
+
+
+
+
+
+
+    while(p1Array!==null && p2Array!==null){
+      if(p1Array.length>p2Array.length){
+        randNum = Math.floor(Math.random()*(p2Array.length));
+      }
+      else{
+        randNum = Math.floor(Math.random()*(p1Array.length));
+      }
+      if(p1Array[randNum].value>p2Array[randNum].value){
+        p1Array.push(p2Array[randNum]);
+        p2Array.splice(randNum, 1);
+      } else if(p1Array[randNum].value<p2Array[randNum].value){
+        p2Array.push(p1Array[randNum]);
+        p1Array.splice(randNum, 1);
+      }
+    }
+    console.log("p1", p1Array);
+    console.log("p2", p2Array);
   });
 });
