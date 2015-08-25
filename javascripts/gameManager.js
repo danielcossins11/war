8fdaa9e77559e6acc14e017aa4b0e3726a526325
danielcossins11 <daniel.cossins@yahoo.com>
2015-08-24 19:32:55 -0500

    dunno what changed

--- javascripts/gameManager.js
+++ javascripts/gameManager.js
@@ -16,34 +16,37 @@ define(function(require){
     });
   });
 
-  for(var i=0; i<cardArray.length; i++){
-    if(cardArray.value==="JACK"){
-      cardArray.value = "11";
-    } else if(cardArray.value==="QUEEN"){
-      cardArray.value = "12";
-    } else if(cardArray.value==="KING"){
-      cardArray.value = "13";
-    } else if(cardArray.value==="ACE"){
-      cardArray.value = "14";
-    }  
-  }
   
 
   $("#draw").on("click", function(){
     console.log(cardArray);
+
+    for(var j=0; j<cardArray.length; j++){
+      if(cardArray[j].value==="JACK"){
+        cardArray[j].value = "11";
+      } else if(cardArray[j].value==="QUEEN"){
+        cardArray[j].value = "12";
+      } else if(cardArray[j].value==="KING"){
+        cardArray[j].value = "13";
+      } else if(cardArray[j].value==="ACE"){
+        cardArray[j].value = "14";
+      }
+    }
+
     for(var i=0; i<52; i++){
       var num;
       if(i%2===0){
-      num = Math.floor(Math.random()*(cardArray.length));
-      p1Array[i] = cardArray[num];
-      cardArray.splice(num, 1);
+        num = Math.floor(Math.random()*(cardArray.length));
+        p1Array[p1Array.length] = cardArray[num];
+        cardArray.splice(num, 1);
       } else{
         num = Math.floor(Math.random()*(cardArray.length));
-        p2Array[i] = cardArray[num];
+        p2Array[p2Array.length] = cardArray[num];
         cardArray.splice(num, 1);
       }
     }
     console.log(cardArray, p1Array, p2Array);
+    console.log(p1Array[0].value);
 
 
 
@@ -53,18 +56,21 @@ define(function(require){
 
 
     while(p1Array!==null && p2Array!==null){
-      if(p1Array.length>p2Array.length){
-        randNum = Math.floor(Math.random()*(p2Array.length));
+      var length;
+      if(p1Array.length>=p2Array.length){
+        length = p2Array.length;
       }
       else{
-        randNum = Math.floor(Math.random()*(p1Array.length));
+        length = p1Array.length;
       }
-      if(p1Array[randNum].value>p2Array[randNum].value){
-        p1Array.push(p2Array[randNum]);
-        p2Array.splice(randNum, 1);
-      } else if(p1Array[randNum].value<p2Array[randNum].value){
-        p2Array.push(p1Array[randNum]);
-        p1Array.splice(randNum, 1);
+      for(var k=0; k<length; k++){
+        if(p1Array[k].value>p2Array[k].value){
+          p1Array.push(p2Array[k]);
+          p2Array.splice(k, 1);
+        } else if(p2Array[k].value>p1Array[k].value){
+          p2Array.push(p1Array[k]);
+          p1Array.splice(k, 1);
+        }
       }
     }
     console.log("p1", p1Array);
